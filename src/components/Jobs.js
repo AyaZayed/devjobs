9990f15b1bf097fa7db91c85b129d47dd7a83b37
Aya Zayed <ayazayed@std.mans.edu.eg>
2023-07-30 01:12:15 +0300

    pagination

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,16 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Context } from '../context'
 
 export default function Jobs() {
     const { state } = useContext(Context)
-    const { filteredJobs } = state
+    const { filteredJobs, loading } = state
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const jobsPerPage = 10
+    const pages = Math.ceil(filteredJobs.length / jobsPerPage)
+
+    function handleNext() {
+        if (currentPage === pages) return
+        setCurrentPage(currentPage + 1)
+    }
+
     return (
         <div className="jobs">
-            {filteredJobs.map(job => (
-                <div className="job" key={job.id}>
-                    {job.location}
-                </div>
-            ))}
+            <div className='pagination'>
+                <button disabled={currentPage < 2} className='previous-page' onClick={() => setCurrentPage(currentPage - 1)}>
+                    before
+                </button>
+                <h6>{pages} / <span className='current'>{currentPage}</span></h6>
+                <button disabled={currentPage == pages} className='next-page' onClick={handleNext}>
+                    next
+                </button>
+            </div>
+            {loading ? 'Loading...' :
+                filteredJobs.slice((currentPage - 1) * jobsPerPage, currentPage * jobsPerPage).map(job => (
+                    <div className="job" key={job.id}>
+                        {job.location}
+                    </div>
+                ))}
         </div>
     )
 }
